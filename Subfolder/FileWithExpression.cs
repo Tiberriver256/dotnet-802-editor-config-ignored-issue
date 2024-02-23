14c4ae2b959c577ee96aceb9139b3328b3802d0c
Micah Rairdon <mrairdon@midmark.com>
2024-02-23 10:13:44 -0500

    Repro editorconfig issue

---
+++ Subfolder/FileWithExpression.cs
@@ -0,0 +1,9 @@
+namespace Dotnet802EditorConfigIgnoredIssue.Subfolder;
+
+internal class FileWithExpression
+{
+    public static void MethodWithExpression()
+    {
+        Console.WriteLine("This is a method with an expression body.");
+    }
+}
